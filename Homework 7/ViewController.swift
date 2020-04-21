84f2611a2791451233d42681255b22ff76c26df4
sb <sb>
2020-04-21 13:13:43 -0400

    done

--- Homework 7/ViewController.swift
+++ Homework 7/ViewController.swift
@@ -7,12 +7,80 @@
 //
 
 import UIKit
+import CoreLocation
+class ViewController:UIViewController, CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    // Seton Hill University
+    let kfloridaLatitude: CLLocationDegrees = 27.994
+    let kfloridaLongitude: CLLocationDegrees = -81.76
 
+    
+
+    
+    
+    
+    
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let newLocation: CLLocation=locations[0]
+        NSLog("Something is happening")
+        
+        // horizontal accuracy less than than 0 means failure at gps level
+        if newLocation.horizontalAccuracy >= 0 {
+            
+            
+            let shu:CLLocation = CLLocation(latitude: kfloridaLatitude,longitude: kfloridaLongitude)
+
+            let delta:CLLocationDistance = shu.distance(from: newLocation)
+            
+            let miles: Double = (delta * 0.000621371) + 0.5 // meters to rounded miles
+            
+            if miles < 3 {
+                // Stop updating the location
+                locMan.stopUpdatingLocation()
+                // Congratulate the user
+                distanceLabel.text = "Enjoy\nfloria!"
+            } else {
+                let commaDelimited: NumberFormatter = NumberFormatter()
+                commaDelimited.numberStyle = NumberFormatter.Style.decimal
+                
+                distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to florida"
+            }
+            
+            
+        }
+        else
+        {
+            // add action if error with GPS
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locMan.delegate = self
+               locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+               locMan.distanceFilter = 1609; // a mile (in meters)
+               locMan.requestWhenInUseAuthorization() // verify access to gps
+               locMan.startUpdatingLocation()
+               startLocation = nil
+        
     }
 
 
